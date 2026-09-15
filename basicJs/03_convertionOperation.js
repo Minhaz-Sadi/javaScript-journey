e54c1e34ba2b5864fba88b844cf562dda6e2b8d3
Md. Minhaz Uddin Sadi <infosadi243204@gmail.com>
2026-09-15 18:10:38 +0000

    opertion

--- basicJs/03_convertionOperation.js
+++ basicJs/03_convertionOperation.js
@@ -29,3 +29,52 @@ let stringNumber = String(someNumber);
 
 console.log(stringNumber); //33
 console.log(typeof stringNumber); //string
+
+
+// ===========Operrations==============
+
+let value = 3;
+let negValue = -value;
+//console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**2); // power
+
+let str1 = "hello";
+let str2 = ", Sadi";
+
+// let str3 = str1 + str2;
+// console.log(str3);
+
+
+// console.log("1" + 2); // string priority  
+// console.log(1 + "2");
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32
+
+// console.log((3 + 4)* 5 % 3);
+
+console.log(true);
+console.log(+true); // 1
+//console.log(true+); // wrong
+
+console.log(+""); // 0
+
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+// gameCounter++;
+// console.log(gameCounter); //101
+++gameCounter;
+console.log(gameCounter); // 101
+
+
+
+
+
+
