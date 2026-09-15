3cbad15272b6c46f49d3a90df0629a7f4a17c648
Md. Minhaz Uddin Sadi <infosadi243204@gmail.com>
2026-09-15 15:59:29 +0000

    file add

---
+++ basicJs/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 1234; // not assign again
+let accountEmail = "sadi@gmail.com"; // not declear again
+var accountPassword = "1345";
+accountCity = "Hathazari";
+let accontState;
+
+//accountId = 2; //does not work for const
+accountEmail = "karim@gmail.com";
+accountPassword = "2342134";
+accountCity = "modunagat";
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accontState]);
+
+/* var prefer not to use*/
---
+++ basicJs/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3 + 3); // we are using nodejs, not browser
+
+
+console.log(3+3);
+console.log("Sadi");
+
+let name = "sadi";
+let age = 20;
+let isLoggedIn = false;
+let state;
+
+// number  --> 2^53
+// bigint 
+// string --> ""
+// boolean --> true / false
+// null --> standalone value
+// undifined --> not assined value
+// symbol --> unique
+
+
+// object
+
+console.log(typeof "Sadi");
+console.log(typeof null); //object
+console.log(typeof undefined); //undifined
+
