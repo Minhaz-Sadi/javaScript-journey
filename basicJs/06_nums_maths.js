510f794d7bbbca501fb43b63866f0af60700477e
Md. Minhaz Uddin Sadi <infosadi243204@gmail.com>
2026-09-16 13:54:25 +0000

    nums and maths

---
+++ basicJs/06_nums_maths.js
@@ -0,0 +1,45 @@
+const score = 500;
+// console.log(score);
+
+const balance = new Number(100);
+// console.log(balance);
+
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2)); // 100.00
+
+const otherNum = 1123.8943;
+
+// console.log(otherNum.toPrecision(4));
+
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString()); // 1,000,000
+// console.log(hundreds.toLocaleString('en-IN')); // 1,000,000 --> indian version
+
+
+
+
+// ===========Maths====================
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.6));
+// console.log(Math.min(4, 3, 6, 7));
+// console.log(Math.max(4, 3, 6, 7));
+
+
+// console.log(Math.random()); //  between 0 and 1
+// console.log(Math.random()*10 + 1);
+// console.log(Math.floor((Math.random()*10) + 1));
+
+const min = 10;
+const max = 20;
+
+// Math.floor(Math.random() * (max - min + 1)) + min;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
