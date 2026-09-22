7e575b8bfcac9afaa1af18440732cac1993a3b83
Md. Minhaz Uddin Sadi <infosadi243204@gmail.com>
2026-09-22 15:33:44 +0000

    chaged folder name

--- p4-ControlFlow/03_truthy-falsy.js
+++ p4-ControlFlow/03_truthy-falsy.js
@@ -23,11 +23,34 @@ if(userMail.length === 0){
     console.log("array is empty");
 }
 
-cons emtyObj = {};
+const emtyObj = {};
 
 if(Object.keys(emtyObj).length === 0){
     console.log("pbject is empty");
-    
 }
+
+// Nullish Coalescing Operation (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10;
+// val1 = null ?? 10;
+// val1 = undefined ?? 15;
+val1 = null ?? 10 ?? 20;
+
+console.log(val1);
+
+
+// Terniary Operator
+
+// conditon ? true : false 
+
+const price = 200;
+
+// price >= 80 ? console.log("yes") : console.log("No");
+price >= 220 ? console.log("yes") : console.log("No");
+
+
+
+
 
 // node basicJS-p4/03_truthy-falsy.js
