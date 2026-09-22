3444178431a3b3fc2a897bd37a360ec4d1459b7d
Md. Minhaz Uddin Sadi <infosadi243204@gmail.com>
2026-09-22 17:49:34 +0000

    loops

---
+++ p5-Loops/02_whileDo.js
@@ -0,0 +1,33 @@
+
+
+let i = 0;
+
+// while ( i <= 10){
+//     console.log(`value: ${i}`);
+//     i = i + 2;
+// }
+
+
+let myArray = ["flash", "batman", "superman"];
+
+// console.log(myArray.length);
+
+// let arr = 0;
+// while( arr < myArray.length ){
+//     console.log(`value: ${myArray[arr]}`);
+//     arr++;
+// }
+
+// DO WHILE LOOP
+
+let score = 11; // printed 11, bcz 1st print then check condition
+
+do {
+
+    console.log(`score is ${score}`);
+    score++;
+    
+} while (score <= 10);
+
+
+// node p5-Loops/02_whileDo.js
---
+++ p5-Loops/04_for_in.js
@@ -0,0 +1,45 @@
+// for in
+
+const myObject = {
+    js: 'javascript',
+    cpp: 'c++',
+    rb: "ruby"
+};
+
+// for (const key in myObject) {
+   
+//    // console.log(key);
+//     // console.log(myObject[key]);
+
+//     console.log(`${key} shortcut is for ${myObject[key]}`);
+    
+// }
+
+const programming = ["js", "rb", "py", "java"];
+
+// for (const key in programming) {
+    
+//     //console.log(key); // 0, 1 ,2 3
+
+//     console.log(programming[key]);
+// }
+
+
+
+
+// ==== map ====
+
+// const map = new Map();
+// map.set('IN', "India");
+// map.set('USA', "United Sates of America");
+// map.set('Fr', "France");
+
+// for(const key in map){
+//     console.log(key); // notihing shows
+// }
+
+
+
+
+
+
